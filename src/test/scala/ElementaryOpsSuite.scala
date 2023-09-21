dec41ce1b9906c7b32b95835cc7313cde14db43f
Dustin Thomas <stdio@cptlobster.dev>
2023-09-21 17:36:53 -0500

    add additional tests for elementary row operations

--- src/test/scala/ElementaryOpsSuite.scala
+++ src/test/scala/ElementaryOpsSuite.scala
@@ -5,13 +5,18 @@ import org.scalatest.funsuite.AnyFunSuite
 class ElementaryOpsSuite extends AnyFunSuite {
   test("Row swap works") {
     assert(ElementaryOps(3).swap(0,1).mat.cell(0, 1) == 1)
+    assert(ElementaryOps(3).swap(2,0).mat.cell(0, 2) == 1)
+    assert(ElementaryOps(3).swap(0,2).mat.cell(0, 2) == 1)
   }
 
   test("Scalar multiply works") {
     assert(ElementaryOps(3).mult(0, 5).mat.cell(0, 0) == 5)
+    assert(ElementaryOps(3).mult(2, 3).mat.cell(2, 2) == 3)
   }
 
   test("Addition works") {
     assert(ElementaryOps(3).add(2, 0).mat.cell(0, 2) == 1)
+    assert(ElementaryOps(3).add(1, 0, 3).mat.cell(0, 1) == 3)
+    assert(ElementaryOps(3).add(2, 1, 4).mat.cell(1, 2) == 4)
   }
 }
